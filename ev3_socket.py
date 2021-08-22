9b46570a2e2cc643d86ccddb3f72706037dd0971
byunkyungho <65907318+byunkyunho@users.noreply.github.com>
2021-08-22 15:18:03 +0900

    Update ev3_socket.py

--- ev3_socket.py
+++ ev3_socket.py
@@ -2,21 +2,20 @@ import socket
 
 class socket_handler:
     def __init__(self, HOST, PORT):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind((HOST, PORT))
-        self.s.listen()
-        self.s = self.s.accept()[0]
-
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((HOST, PORT))
+        self.sock.listen()
+        self.sock = self.sock.accept()[0]
 
     def send_message(self, message):
-        self.s.send(message.encode())
+        self.sock.send(message.encode())
 
     def recive_message(self):
-        return self.s.recv(1024).decode()
+        return self.sock.recv(1024).decode()
         
 class cube_robot:
     def __init__(self):
-        IP = "192.168.1.26"
+        IP = "xxx.xxx.xxx.xxx"
         FIRST_PORT = 5001
         SECOND_PORT = 5000
         print("start_first_connect")
@@ -34,7 +33,6 @@ class cube_robot:
         return False
 
     def rotate(self, rotation):
-        
         if isinstance(rotation, list):
             for spin in rotation:
                 if self.check_side(spin):
